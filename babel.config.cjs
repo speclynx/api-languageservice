4b2947b1c314c7b6e2898c6878929023b75cb675
Vladimír Gorej <vladimir.gorej@gmail.com>
2026-07-07 12:50:57 +0100

    chore: upgrade Babel toolchain to v8 (#176)
    Bumps @babel/cli, @babel/core, @babel/plugin-transform-runtime,
    @babel/preset-env, @babel/preset-typescript to v8 and
    @babel/runtime-corejs3 to v8. Also bumps babel-loader to 10.1.1 for
    Babel 8 peer compatibility. Adjusts babel.config.cjs for breaking
    changes: drops removed preset-env loose/useBuiltIns options, drops
    removed preset-typescript allowDeclareFields option, sets
    onlyRemoveTypeImports: false to preserve Babel 7's usage-based
    type-import elision, and migrates transform-runtime from
    corejs/helpers/regenerator options to the new moduleName option.
    Updates the vendored babel-plugin-add-import-extension version
    assertion to Babel 8.

--- babel.config.cjs
+++ babel.config.cjs
@@ -12,29 +12,20 @@ module.exports = {
           {
             debug: false,
             modules: 'commonjs',
-            loose: true,
-            useBuiltIns: false,
             forceAllTransforms: false,
             ignoreBrowserslistConfig: false,
             exclude: ['transform-function-name'],
           },
         ],
-        [
-          '@babel/preset-typescript',
-          {
-            allowDeclareFields: true,
-          },
-        ],
+        ['@babel/preset-typescript', { onlyRemoveTypeImports: false }],
       ],
       plugins: [
         [
           '@babel/plugin-transform-runtime',
           {
-            corejs: { version: 3, proposals: false },
+            moduleName: '@babel/runtime-corejs3',
             absoluteRuntime: false,
-            helpers: true,
-            regenerator: false,
-            version: '^7.22.15',
+            version: '^8.0.0',
           },
         ],
         process.env.NODE_ENV !== 'test'
@@ -56,29 +47,20 @@ module.exports = {
           {
             debug: false,
             modules: false,
-            useBuiltIns: false,
             forceAllTransforms: false,
             ignoreBrowserslistConfig: false,
             exclude: ['transform-function-name'], // this is here because of https://github.com/babel/babel/discussions/12874
           },
         ],
-        [
-          '@babel/preset-typescript',
-          {
-            allowDeclareFields: true,
-          },
-        ],
+        ['@babel/preset-typescript', { onlyRemoveTypeImports: false }],
       ],
       plugins: [
         [
           '@babel/plugin-transform-runtime',
           {
-            corejs: { version: 3, proposals: false },
+            moduleName: '@babel/runtime-corejs3',
             absoluteRuntime: false,
-            helpers: true,
-            regenerator: false,
-            useESModules: true,
-            version: '^7.22.15',
+            version: '^8.0.0',
           },
         ],
         [
@@ -98,28 +80,20 @@ module.exports = {
           {
             debug: false,
             modules: 'auto',
-            useBuiltIns: false,
             forceAllTransforms: false,
             ignoreBrowserslistConfig: false,
             exclude: ['transform-function-name'], // this is here because of https://github.com/babel/babel/discussions/12874
           },
         ],
-        [
-          '@babel/preset-typescript',
-          {
-            allowDeclareFields: true,
-          },
-        ],
+        ['@babel/preset-typescript', { onlyRemoveTypeImports: false }],
       ],
       plugins: [
         [
           '@babel/plugin-transform-runtime',
           {
-            corejs: { version: 3, proposals: false },
+            moduleName: '@babel/runtime-corejs3',
             absoluteRuntime: false,
-            helpers: true,
-            regenerator: false,
-            version: '^7.22.15',
+            version: '^8.0.0',
           },
         ],
         process.env.OBFUSCATE === 'true'
